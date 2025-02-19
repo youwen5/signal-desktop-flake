d181d3d0b7e813f48792421b4b6ca93b2c51c6a3
Youwen Wu <youwenw@gmail.com>
2025-02-18 18:02:17 -0800

    initial commit

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "Signal desktop flake with up to date aarch64-linux package";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      forAllSystems =
+        nixpkgs.lib.genAttrs
+          nixpkgs.legacyPackages."aarch64-linux".signal-desktop.meta.platforms;
+    in
+    {
+
+      packages = forAllSystems (
+        system:
+
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        {
+          signal-desktop =
+            if pkgs.stdenv.targetPlatform.isAarch then
+              pkgs.callPackage ./signal-desktop.nix { }
+            else
+              pkgs.signal-desktop;
+          default = self.packages.${system}.signal-desktop;
+        }
+      );
+    };
+}
